135aa9a1551ca07906012165b208b1e4bd59d5bb
Paolo Brasolin <paolo.brasolin@gmail.com>
2016-04-23 18:45:35 +0200

    filter code cleanup

--- filters/custom-tables.hs
+++ filters/custom-tables.hs
@@ -1,32 +1,16 @@
 import Text.Pandoc
 import Text.Pandoc.JSON
 
-{--
-main = toJSONFilter inlineLatex
-  where inlineLatex (Emph xs) =
-          [latex "\\Emph{"  ] ++ xs ++ [latex "}"]
-        inlineLatex (Strong xs) =
-          [latex "\\Strong{"] ++ xs ++ [latex "}"]
-        inlineLatex s@(Span (id, as, kps) xs) =
-          case as of
-            ["tag"] -> [latex "\\ParTag{"] ++ xs ++ [latex "}"]
-            ["basedon"] -> [latex "\\BasedOn{"] ++ xs ++ [latex "}"]
-            _ -> [s]
-        inlineLatex x = [x]
-        latex = RawInline (Format "latex")
---}
-
-
 main = toJSONFilter customTable
   where customTable (Table caption aligns widths heads rows) =
           [Table caption aligns (killWidths widths) heads rows]
         customTable x = [x]
-        rawtex = RawBlock (Format "latex")
         killWidths a = map f a
           where f _ = 0.0
-        doAligns a = concat $ map f a
-          where f AlignLeft    = "l"
-                f AlignRight   = "r"
-                f AlignCenter  = "c"
-                f AlignDefault = "l"
+--        rawtex = RawBlock (Format "latex")
+--        doAligns a = concat $ map f a
+--          where f AlignLeft    = "l"
+--                f AlignRight   = "r"
+--                f AlignCenter  = "c"
+--                f AlignDefault = "l"
 
